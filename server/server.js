70012883483d7e189e1a8f0a54ea90d8e82cbf09
sauravsehgal24 <sauravsehgal44@gmail.com>
2019-12-06 21:37:24 -0500

    added dotenv creds and bootstrap

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3001
+require('dotenv').config();
+
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
