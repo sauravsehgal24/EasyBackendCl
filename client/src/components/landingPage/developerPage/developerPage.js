1088475e298acf5ce39ffbf25c4b46ffcfc4dd42
sauravsehgal24 <sauravsehgal44@gmail.com>
2020-01-16 10:35:56 -0500

    updated developers page

--- client/src/components/landingPage/developerPage/developerPage.js
+++ client/src/components/landingPage/developerPage/developerPage.js
@@ -1,12 +1,44 @@
-import React, {Component} from 'react';
-import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
-
+import React, { Component } from "react";
+import { Route, Link, BrowserRouter as Router } from "react-router-dom";
+import { Container, Row, Col, Card } from "react-bootstrap";
+import "./developerPage.css";
 export default function DeveloperPage() {
-
-        return (
-            <React.Fragment>
-                <h1>Developer Page</h1>
-            </React.Fragment>
-        );
-  
+  return (
+    <React.Fragment>
+      <Container className="containerDeveloperPage" fluid>
+        <Card className="developerPageCard">
+          <Card.Body className="developerPageCardBody">
+            <header className="cardTitle">Hey Developers Whats Up!</header>
+            <hr className="divider"></hr>
+            <img
+              src={require("../../../assets/images/myimg.png")}
+              className="myProfilePic"
+            />
+            <div className="developersCardPara">
+              My name is <strong>Saurav Sehgal</strong> and I am a Full-Stack
+              Developer like most of you out there. I am a recent graduate from
+              Sheridan College (Brampton, ON, CA) and this is my first open
+              source and big project after my studies. After some research and
+              binge watching the documentation for react, I came up with this
+              project. Currently developing some of the major features and being
+              an open source project, you can also contribute via my Github link
+              provided down below.
+            </div>
+            <div className="contactImagesContainer">
+                <a href='https://github.com/sauravsehgal24'>
+              <img
+                src={require("../../../assets/images/github.png")}
+                className="githubPic"
+              /></a>
+              <a href='http://www.linkedin.com/in/saurav-sehgal'>
+              <img
+                src={require("../../../assets/images/linkindn.png")}
+                className="linkindnPic"
+              /></a>
+            </div>
+          </Card.Body>
+        </Card>
+      </Container>
+    </React.Fragment>
+  );
 }
