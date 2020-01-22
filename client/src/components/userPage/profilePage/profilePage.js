9359bc40a6924b9f637c208a1a9697850be87214
Saurav Sehgal <saurav.sehgal@dhltd.corp>
2020-01-22 11:19:38 -0500

    routes added for user dashboard

---
+++ client/src/components/userPage/profilePage/profilePage.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import { Route, Link, BrowserRouter as Router, Switch } from 'react-router-dom';
+
+export default function ProfilePage() {
+
+        return (
+            <React.Fragment>
+                <h1>Profile</h1>
+            </React.Fragment>
+        );
+  
+}
